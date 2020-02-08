b7d85d9a39ab6a44241350fbfdf54a9e025cde5f
Dawar Aziz Ahmad <dawaraziz95@gmail.com>
2020-02-08 18:31:39 -0500

    Added code to AST and Main

--- src/com/project/Parser/ParsTree.java
+++ src/com/project/Parser/ParsTree.java
@@ -9,16 +9,49 @@ public class ParsTree {
     String value = "";
 
     boolean terminal = false;
+    boolean traversed = false;
 
     ArrayList<ParsTree> children = new ArrayList<>();
 
-    ParsTree(final String value, final boolean terminal) {
+    ParsTree parent;
+
+    public ParsTree(final String value, final boolean terminal, final boolean traversed, final ParsTree parent) {
         this.value = value;
         this.terminal = terminal;
+        this.traversed = traversed;
+        this.parent = parent;
     }
 
-    public void addChild(String value, boolean terminal) {
-        this.children.add(new ParsTree(value, terminal));
+    public void addChild(String value, boolean terminal, boolean traversed, ParsTree parent) {
+        this.children.add(new ParsTree(value, terminal, traversed, parent));
+    }
+
+    public ParsTree getChild(int i) {
+        return this.children.get(i);
+    }
+
+    public boolean noChildren() {
+        return this.children.isEmpty();
+    }
+
+    public ArrayList<ParsTree> getChildren() {
+        return this.children;
+    }
+
+    public void setTraversed(boolean traversed) {
+        this.traversed = traversed;
+    }
+
+    public ParsTree getParent() {
+        return this.parent;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public boolean isTerminal() {
+        return this.terminal;
     }
 
 }
