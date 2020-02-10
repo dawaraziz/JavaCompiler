eb1d295d1fc63ce938d864c6b101dd25e2add990
Calum Bruton <calumbruton@gmail.com>
2020-02-10 18:11:42 -0500

    Updated Test Script

--- run_tests.py
+++ run_tests.py
@@ -19,13 +19,13 @@ def main():
     for root, subFolders, files in os.walk(test_dir):
         for folder in subFolders:
             print("\033[0mRunning {} Tests".format(folder))
-            legalTest = True if folder == ("legalJoos") else False
+            illegalTest = True if folder == ("illegalJoos") else False
             for root, subFolders, files in os.walk(test_dir+"/"+folder):
                 for file in files:
                     file_path = "{}/{}/{}".format(test_dir, folder, file)
                     result = subprocess.run(['java', '-jar', 'build/jar/joosc.jar', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
                     retcode = result.returncode
-                    if legalTest:
+                    if not illegalTest:
                         # We expect no failures for the legal tests
                         if retcode:
                             print("\t{2}FAILED - {0}: {1}{2}".format(file, result.stderr, '\033[91m'))
@@ -35,14 +35,14 @@ def main():
                                 print("\t{2}PASSED - {0} {1}{2}".format(file, result.stderr, '\033[92m'))
                             Passed += 1
 
-                    if not legalTest:
+                    else:
                         # Failing is a passed test for the illegal Joos 1W files
                         if retcode:
                             if show_all:
                                 print("\t{2}PASSED - {0}: {1}{2}".format(file, result.stderr, '\033[92m'))
                             Passed += 1
                         else:
-                            print("\t{2}FAILED - {0}: {1}{2}".format(file, "Should have failed!", '\033[91m'))
+                            print("\t{2}ILLEGAL FAILED - {0}: {1}{2}".format(file, "Should have failed!", '\033[91m'))
                             Failed += 1
 
     print("\033[0mTOTAL PASSED TESTS: {}".format(Passed))
