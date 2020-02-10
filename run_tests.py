20af29622e49fa882746d566ccdd4befe339c9f6
Calum Bruton <calumbruton@gmail.com>
2020-02-10 16:50:30 -0500

    Testing Framework and Script

--- run_tests.py
+++ run_tests.py
@@ -1,8 +1,25 @@
+import os
+import sys
+import subprocess
+
 def main():
+    show_all = True if len(sys.argv) > 1 and'-a' in sys.argv[1] else False
 
+    test_dir = "tests"
 
-
-
+    for root, subFolders, files in os.walk(test_dir):
+        for folder in subFolders:
+            print("Running {} Tests".format(folder))
+            for root, subFolders, files in os.walk(test_dir+"/"+folder):
+                for file in files:
+                    file_path = "{}/{}/{}".format(test_dir, folder, file)
+                    result = subprocess.run(['java', '-jar', 'build/jar/joosc.jar', file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+                    retcode = result.returncode
+                    if retcode:
+                        print("\tError in {}: {}".format(file, result.stderr))
+                    else:
+                        if show_all:
+                            print("\tPASSED: {}".format(file))
 
 
 
