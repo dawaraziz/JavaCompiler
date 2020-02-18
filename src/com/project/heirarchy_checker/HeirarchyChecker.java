bdfb8e1b4124f81e5dd0ab98f5d15179822acb05
Dawar Aziz Ahmad <dawaraziz95@gmail.com>
2020-02-18 16:36:59 -0500

    Update HeirarchyChecker

--- src/com/project/heirarchy_checker/HeirarchyChecker.java
+++ src/com/project/heirarchy_checker/HeirarchyChecker.java
@@ -1,4 +1,34 @@
 package com.project.heirarchy_checker;
 
+import com.project.environments.ClassScope;
+import com.project.environments.structure.Name;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class HeirarchyChecker {
+
+    final ArrayList<ClassScope> classTable;
+
+    public HeirarchyChecker(final ArrayList<ClassScope> classTable) {
+        this.classTable = classTable;
+    }
+
+    public boolean classExtendsInterface() {
+        ArrayList<String> interfacesSeen = new ArrayList<>();
+
+        for (ClassScope javaClass : classTable) {
+            if (javaClass.type == ClassScope.CLASS_TYPE.INTERFACE) {
+                interfacesSeen.add((javaClass.name));
+            }
+        }
+
+        for (ClassScope javaClass: classTable) {
+            if (interfacesSeen.contains(javaClass.extendsName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
