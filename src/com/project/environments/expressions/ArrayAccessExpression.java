3e570386afd109aceec071fc1ee3a6e35f72e202
Nantag <piccioni.alfred@gmail.com>
2020-03-10 22:57:15 -0400

    Fixed some more errors.

--- src/com/project/environments/expressions/ArrayAccessExpression.java
+++ src/com/project/environments/expressions/ArrayAccessExpression.java
@@ -37,7 +37,7 @@ public class ArrayAccessExpression extends Expression {
     public void linkTypesToQualifiedNames(final ClassScope rootClass) {
         LHS.linkTypesToQualifiedNames(rootClass);
         RHS.linkTypesToQualifiedNames(rootClass);
-        this.type = LHS.type;
+        this.type = new Type(LHS.type, false);
 
 //        if (!LHS.type.isArray) {
 //            System.err.println("Found array access with non-array LHS.");
