c8c4500d0881f5c8d79bdcbf462f26e51e125307
Calum Bruton <calumbruton@gmail.com>
2020-03-11 13:55:03 -0400

    Added show stdout

--- run_tests_2.py
+++ run_tests_2.py
@@ -17,6 +17,7 @@ def main():
     prefix = sys.argv[1] if len(sys.argv) > 1 else exit('give assignment as first argument ex. run_tests.py a1')
     show_all = True if len(sys.argv) > 1 and'-s' in sys.argv else False
     hierarchy = True if len(sys.argv) > 1 and'-h' in sys.argv else False
+    show_stdout = True if len(sys.argv) > 1 and'-o' in sys.argv else False
     use_prefix = True if len(sys.argv) > 1 and'-p' in sys.argv else False
     if use_prefix:
         ind = sys.argv.index('-p')
@@ -57,7 +58,7 @@ def main():
                                             doH = True
                                 #if (doH):
                             result = subprocess.run(['java', '-jar', 'out/artifacts/cs444_w20_group33_jar/cs444-w20-group33.jar', file_path] + stdLib, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
-                            Passed, Failed = passOrFail(test, result, illegalTest, show_all, Passed, Failed)
+                            Passed, Failed = passOrFail(test, result, illegalTest, show_all, show_stdout, Passed, Failed)
 
                         # If directory compile all files within it
                         else:
@@ -74,7 +75,7 @@ def main():
                                 #if (doH):
                             compilation_unit = ['java', '-jar', 'out/artifacts/cs444_w20_group33_jar/cs444-w20-group33.jar'] + file_paths + stdLib
                             result = subprocess.run(compilation_unit, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
-                            Passed, Failed = passOrFail(test, result, illegalTest, show_all, Passed, Failed)
+                            Passed, Failed = passOrFail(test, result, illegalTest, show_all, show_stdout, Passed, Failed)
 
 
     print("\033[0mTOTAL PASSED TESTS: {}".format(Passed))
@@ -106,26 +107,27 @@ def allFilesInDirAndSubdir(path):
     return files
 
 
-def passOrFail(test, result, illegalTest, show_all, Passed, Failed):
+def passOrFail(test, result, illegalTest, show_all, show_stdout, Passed, Failed):
     retcode = result.returncode
+    stdOut = result.stdout if show_stdout else ""
     if not illegalTest:
         # We expect no failures for the legal tests
         if retcode:
-            print("\t{2}FAILED - {0}: {1}{2}".format(test, result.stderr.strip(), '\033[91m'))
+            print("\t{2}FAILED - {0}: {1}{2}{3}".format(test, result.stderr.strip(), '\033[91m', stdOut))
             Failed += 1
         else:
             if show_all:
-                print("\t{2}PASSED - {0} {1}{2}".format(test, result.stderr.strip(), '\033[92m'))
+                print("\t{2}PASSED - {0} {1}{2}{3}".format(test, result.stderr.strip(), '\033[92m', stdOut))
             Passed += 1
 
     else:
         # Failing is a passed test for the illegal Joos 1W tests
         if retcode:
             if show_all:
-                print("\t{2}PASSED - {0}: {1}{2}".format(test, result.stderr.strip(), '\033[92m'))
+                print("\t{2}PASSED - {0}: {1}{2}{3}".format(test, result.stderr.strip(), '\033[92m', stdOut))
             Passed += 1
         else:
-            print("\t{2}ILLEGAL FAILED - {0}: {1}{2}".format(test, "Should have failed!", '\033[91m'))
+            print("\t{2}ILLEGAL FAILED - {0}: {1}{2}{3}".format(test, "Should have failed!", '\033[91m', stdOut))
             Failed += 1
     return Passed, Failed
 
