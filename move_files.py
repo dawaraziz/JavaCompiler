8797dd62e2fb2809fe1d8c99c6068f13c3821c27
Dawar Aziz Ahmad <dawaraziz95@gmail.com>
2020-04-11 03:36:18 -0400

    Python test scripts

--- move_files.py
+++ move_files.py
@@ -18,7 +18,7 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(server, username=username, password=password)
 
 def move_files(directory):
-	mkdir_cmd = "mkdir /u2/daahmad/cs444/" + directory
+	mkdir_cmd = "mkdir /u2/"+username+"/cs444/" + directory
 	ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(mkdir_cmd)
 
 	assembly_files = os.listdir('./output/' + directory + "/")
@@ -26,7 +26,7 @@ def move_files(directory):
 	for file in assembly_files:
 		if file[-2:] == ".s":
 			localpath = './output/' + directory + "/" + file
-			remotepath = "/u2/daahmad/cs444/" + directory + "/" + file
+			remotepath = "/u2/"+username+"/cs444/" + directory + "/" + file
 			sftp = ssh.open_sftp()
 			sftp.put(localpath, remotepath)
 			sftp.close()
